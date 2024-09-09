f014c62fbbbb19b0e5e6edb4e41d01590ddd9e7b
L Ronnie Nichols <lorraineveronica95@gmail.com>
2024-09-09 16:17:11 -0400

    final project

--- main FM.py
+++ main FM.py
@@ -13,7 +13,7 @@ print(f"Welcome, {name}, which fruit would you like to buy? (1, 2, or 3)")
 print(wares[0])
 print(wares[1])
 print(wares[2])
-fruit1 = input()
+fruit1 = int(input())
 
 if fruit1 == 1:
     total_cost += 2
@@ -25,16 +25,16 @@ elif fruit1 == 2:
 
 elif fruit1 == 3:
     total_cost += 3
-    oranges +=3
+    oranges +=1
 
-fruitnex= input("Would you like to buy another piece of fruit? y/n")
+fruitnex= input("Would you like to buy another piece of fruit? y/n ")
 
 while fruitnex=="y":
     print(f"Welcome, {name}, which fruit would you like to buy? (1, 2, or 3)")
     print(wares[0])
     print(wares[1])
     print(wares[2])
-    fruit1 = input()
+    fruit1 = int(input())
 
     if fruit1 == 1:
         total_cost += 2
@@ -46,4 +46,14 @@ while fruitnex=="y":
         total_cost += 3
         oranges += 1
 
-fruitnex= input("Would you like to buy another piece of fruit? y/n")
+    fruitnex= input("Would you like to buy another piece of fruit? y/n ")
+
+print (f"Order for {name}")
+print (f"{apples} apple(s) at $2 each")
+print (f"{grapes} grape(s) at $1 each")
+print (f"{oranges} orange(s) at $3 each")
+print (f"subtotal: ${total_cost}")
+tax= .05 * total_cost
+final_cost= tax + total_cost
+print( f"tax: ${tax}")
+print (f"total: ${final_cost}")
